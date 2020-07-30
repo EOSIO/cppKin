2092121913f9333de2acc3cf29fb308f063daec9
Emory Barlow <emory.barlow@block.one>
2020-07-30 16:16:06 -0400

    Print trace id on submit

--- src/Span.cpp
+++ src/Span.cpp
@@ -107,6 +107,7 @@ namespace cppkin
         m_span->SetEndTime();
         if(strcmp(value, Annotation::Value::NOP) != 0)
             AddAnnotation(value, m_span->GetTimeStamp() + m_span->GetDuration());
+        std::cerr << "Trace ID: " << m_span->GetHeader().TraceID << std::endl;
         TransportManager::Instance().PushSpan(m_span);
     }
 
